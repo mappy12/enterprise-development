f59d4a89a85a92276f782078df854df94b39ee83
Andrey <mappydesuu@gmail.com>
2026-09-22 14:46:53 +0400

    добавил summary к элементам перечисления

--- Hotel/Hotel.Domain/Shared/RoomCategory.cs
+++ Hotel/Hotel.Domain/Shared/RoomCategory.cs
@@ -5,8 +5,23 @@
 /// </summary>
 public enum RoomCategory
 {
+    /// <summary>
+    /// Экономичная категория номера гостиницы
+    /// </summary>
     Economy,
+
+    /// <summary>
+    /// Стандартная категория номера гостиницы
+    /// </summary>
     Standard,
+
+    /// <summary>
+    /// Комфортная категория номера гостиницы
+    /// </summary>
     Comfort,
+
+    /// <summary>
+    /// Люксовая категория номера гостиницы
+    /// </summary>
     Luxury
 }
